e8d5b830fd95d1173a2f9b57d37c987b71ed67b8
Morison <morisonabinsinguza@gmail.com>
2025-04-08 18:15:18 +0300

    Updating settings

--- aits/settings.py
+++ aits/settings.py
@@ -99,10 +99,14 @@ WSGI_APPLICATION = 'aits.wsgi.application'
 
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
+db_path = tmpPostgres.path
+if isinstance(db_path, bytes):
+    db_path = db_path.decode('utf-8')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': db_path.replace('/', ''),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
